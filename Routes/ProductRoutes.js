2cd560f24a19eccff11f3339e2b3b9a99cb2bcad
pavanvasala-123 <pavanvasalaa@gmail.com>
2024-07-16 13:55:37 +0530

    updated auth middleware

--- Routes/ProductRoutes.js
+++ Routes/ProductRoutes.js
@@ -4,19 +4,21 @@ const {
   getProducts,
   updateProduct,
   deleteProduct,
+ 
 } = require("../Controllers/ProductController");
 const {
   auth,
   admin,
   manager,
+  adminOrManager,
   noStaff,
 } = require("../Auth Middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", auth, noStaff, admin, createProduct);
-router.get("/", auth, noStaff, admin, manager, getProducts);
-router.put("/:id", auth, noStaff, admin, manager, updateProduct);
+router.get("/", auth, noStaff, adminOrManager, getProducts);
+router.put("/:id", auth, noStaff, adminOrManager, updateProduct);
 router.delete("/:id", auth, noStaff, admin, deleteProduct);
 
 module.exports = router;
